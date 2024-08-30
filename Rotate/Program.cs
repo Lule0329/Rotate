684371926e81fcc1b708f83bb2c9f52091410f7d
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-08-30 12:27:52 +0200

    Add project files.

---
+++ Rotate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rotate
+            string[] rotateTestData = { "a", "b", "c", "d", "e" };
+            Console.WriteLine("Demonstration av Rotate:");
+            Console.WriteLine("Före: " + string.Join(",", rotateTestData));
+            string[] rotateTestDataResult = Rotate(rotateTestData, 2);
+            Console.WriteLine("Efter: " + string.Join(",", rotateTestDataResult));
+        }
+
+        public static string[] Rotate(string[] strings, int n)
+        {
+            string[] result = new string[strings.Length];
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (i + n > strings.Length - 1)
+                {
+                    result[i] = strings[i % n];
+                } 
+                else if (i + n < strings.Length)
+                {
+                    result[i] = strings[i + n];
+                }
+            }
+
+            return result;
+        }
+    }
+}
